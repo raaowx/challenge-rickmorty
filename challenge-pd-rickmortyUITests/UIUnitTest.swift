68158bd29ae463736406cb50012d43d713ca2c71
raaowx <raaowx@protonmail.com>
2021-08-11 17:11:05 +0200

    [ADD] Character, Location model [ADD] Model decode init unit tests

---
+++ challenge-pd-rickmortyUITests/UIUnitTest.swift
@@ -0,0 +1,18 @@
+//
+//  UIUnitTest.swift
+//  challenge-pd-rickmortyUITests
+//
+//  Created by raaowx on 10/8/21.
+//
+
+import XCTest
+
+class UIUnitTest: XCTestCase {
+  func testAppLaunch() throws {
+    // UI tests must launch the application that they test.
+    let app = XCUIApplication()
+    app.launch()
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
+}
