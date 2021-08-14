75320f49f819dac59e7157f76003e9ae8decbe44
raaowx <raaowx@protonmail.com>
2021-08-14 17:14:42 +0200

    [ADD] UI Tests

--- challenge-pd-rickmortyUITests/UIUnitTest.swift
+++ challenge-pd-rickmortyUITests/UIUnitTest.swift
@@ -15,4 +15,28 @@ class UIUnitTest: XCTestCase {
     // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
   }
+
+  func testScrollListThreeTimes() throws {
+    let app = XCUIApplication()
+    app.launch()
+    let table = app.tables.element
+    let visibleCells = table.cells
+    for _ in 0..<3 {
+      table.swipeUp()
+    }
+    XCTAssertFalse(visibleCells == table.cells)
+  }
+
+  func testSearchBar() throws {
+    let app = XCUIApplication()
+    app.launch()
+    let searchBar = app.searchFields.element
+    searchBar.tap()
+    app.keys["s"].tap()
+    app.keys["m"].tap()
+    app.keys["i"].tap()
+    app.keys["t"].tap()
+    app.keys["h"].tap()
+    XCTAssertFalse((searchBar.value as? String)?.isEmpty ?? true)
+  }
 }
